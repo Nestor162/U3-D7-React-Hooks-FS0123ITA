cd64c296be74b51d53022a4106cfd8fcf1dcf274
Nestor162 <nestorcicardini@gmail.com>
2023-04-04 15:52:41 +0200

    Refactor componente

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -1,22 +1,20 @@
-import { Component } from "react";
+import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 
-class AddComment extends Component {
-  state = {
-    commentObj: {
-      comment: "",
-      rate: "1",
-      elementId: this.props.asin
-    }
-  };
+const AddComment = props => {
+  const [commentObj, setCommentObj] = useState({
+    comment: "",
+    rate: "1",
+    elementId: props.asin
+  });
 
-  sendComment = async e => {
+  const sendComment = async e => {
     e.preventDefault();
 
     try {
       const response = await fetch("https://striveschool-api.herokuapp.com/api/comments/", {
         method: "POST",
-        body: JSON.stringify(this.state.commentObj),
+        body: JSON.stringify(commentObj),
         headers: {
           "Content-Type": "application/json",
           Authorization:
@@ -24,19 +22,12 @@ class AddComment extends Component {
         }
       });
       if (response.ok) {
-        // alert("Commento inviato");
+        props.fetchComments();
 
-        // ricrea la lista di commenti nel livello superiore (il padre: CommentArea)
-        // attraverso la referenza della funzione fetchComments passata come prop a questo componente
-        this.props.fetchComments();
-
-        // resetta i campi (svuotarli)
-        this.setState({
-          commentObj: {
-            comment: "",
-            rate: "1",
-            elementId: this.props.asin
-          }
+        setCommentObj({
+          comment: "",
+          rate: "1",
+          elementId: props.asin
         });
       }
     } catch (error) {
@@ -44,45 +35,41 @@ class AddComment extends Component {
     }
   };
 
-  render() {
-    return (
-      <Form onSubmit={this.sendComment}>
-        <Form.Group className="mb-3" controlId="comment">
-          <Form.Label>Commento</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Inserisci il commento"
-            value={this.state.commentObj.comment}
-            onChange={e => this.setState({ commentObj: { ...this.state.commentObj, comment: e.target.value } })}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="comment">
-          <Form.Label>Voto</Form.Label>
-          <Form.Select
-            value={this.state.commentObj.rate}
-            onChange={e => {
-              this.setState({
-                commentObj: {
-                  ...this.state.commentObj,
-                  rate: e.target.value
-                }
-              });
-            }}
-          >
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </Form.Select>
-        </Form.Group>
+  return (
+    <Form onSubmit={sendComment}>
+      <Form.Group className="mb-3" controlId="comment">
+        <Form.Label>Commento</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Inserisci il commento"
+          value={commentObj.comment}
+          onChange={e => setCommentObj({ ...commentObj, comment: e.target.value })}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="comment">
+        <Form.Label>Voto</Form.Label>
+        <Form.Select
+          value={commentObj.rate}
+          onChange={e =>
+            setCommentObj({
+              ...commentObj,
+              rate: e.target.value
+            })
+          }
+        >
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </Form.Select>
+      </Form.Group>
 
-        <Button type="submit" variant="primary">
-          Invia commento
-        </Button>
-      </Form>
-    );
-  }
-}
+      <Button type="submit" variant="primary">
+        Invia commento
+      </Button>
+    </Form>
+  );
+};
 
 export default AddComment;
